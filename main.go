952cc509ae824c1218c6e3c672d0b92de3ba208d
Tudor Hulban <prindex2010@gmail.com>
2022-09-22 10:42:53 +0300

    added http endpoint

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "alive")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
 
 func main() {
-	fmt.Println("alive")
+	handleRequests()
 }
